495d075c3033a3916045ae8c83b93d76ed1ca774
Aldo Valencia <aldo66662@gmail.com>
2022-11-07 14:48:17 -0500

    primer commit

---
+++ lib/pages/card_page.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text("Card Page"),
+      ),
+      body: Column(
+        children: [
+          //Card 1
+
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(14.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  offset: Offset(4, 4),
+                  blurRadius: 12.0,
+                ),
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.6),
+                  offset: Offset(-4, -4),
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Image .asset(
+                  "assets/images/perfil.png",
+                  height: 50.0,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        "John Doe",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7.0,
+                      ),
+                      Text(
+                        "Ceo at Apple inc.",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 4,
+                        style: TextStyle(
+                          color: Colors.black45,
+                          //fontSize: 16.0,
+                          //fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Icon(Icons.settings, color: Colors.blue,)),
+                Text(
+                  "Settings",
+                  style: TextStyle(color: Colors.blue),),
+              ],
+            ),
+          ),
+
+          //Card 2
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(14.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  offset: Offset(4, 4),
+                  blurRadius: 12.0,
+                ),
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.6),
+                  offset: Offset(-4, -4),
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/vaso.png",
+                  height: 50.0,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        "The  quick, brown fox jumps over",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7.0,
+                      ),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consetur sadiscing elitr, sed diam nonumy eirmod tempor.",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 4,
+                        style: TextStyle(
+                          color: Colors.black45,
+                          //fontSize: 16.0,
+                          //fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+
+          // Card 3
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  offset: Offset(4, 4),
+                  blurRadius: 12.0,
+                ),
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.6),
+                  offset: Offset(-4, -4),
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    " loren ipsum dolor sit amet, consetetur"
+                    "Lorem ipsum dolor sit amet, consetetur ",
+                    maxLines: 6,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: Image.asset(
+                    "assets/images/encendido.png",
+                    height: 120.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
